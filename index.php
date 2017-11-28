f6f78659a09063743f2432cf008c605673ef85da
alexflp <it@flash.ca>
2017-11-28 15:59:40 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,289 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>AlphaPay</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,700|Montserrat:400,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<header role="banner" id="fh5co-header">
+			<div class="container">
+				<!-- <div class="row"> -->
+			    <nav class="navbar navbar-default">
+		        <div class="navbar-header">
+		        	<!-- Mobile Toggle Menu Button -->
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+		          	<a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a> 
+		        </div>
+		        <div id="navbar" class="navbar-collapse collapse">
+		          <ul class="nav navbar-nav navbar-right">
+		            <li class="active"><a href="#" data-nav-section="home"><span>Home</span></a></li>
+		            <li><a href="#" data-nav-section="services"><span>Services</span></a></li>
+		            <li><a href="#" data-nav-section="screenshots"><span>Screenshots</span></a></li>
+		            <li><a href="#" data-nav-section="tour"><span>Live Demo</span></a></li>
+		      
+		            
+		          </ul>
+		        </div>
+			    </nav>
+			  <!-- </div> -->
+		  </div>
+	</header>
+
+	<div id="slider" data-section="home">
+		<div class="owl-carousel owl-carousel-main owl-carousel-fullwidth">
+			<!-- You may change the background color here. -->
+		    <div class="item" style="background: #E75927;">
+		    	<div class="container" style="position: relative;">
+		    		<div class="row animate-box" data-animate-effect="fadeIn">
+					    <div class="col-md-7 col-sm-7">
+			    			<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+						    		<h1 class="fh5co-lead ">Mobile Payment</h1>
+									<h2 class="fh5co-sub-lead ">Grow Up Your Business With Ali Pay <br> And Wechat Pay. </h2>
+								
+						    	</div>
+						    </div>
+					    </div>
+					    <div class="col-md-4 col-md-push-1 col-sm-4 col-sm-push-1 mobile-image">
+					    	<div class="mobile"><img src="images/phone.png" alt="phone"></div>
+					    </div>
+
+		    		</div>
+		    	</div>
+		    </div>
+			<!-- You may change the background color here.  -->
+		    <div class="item" style="background: #E75927;">
+		    	<div class="container" style="position: relative;">
+		    		<div class="row animate-box" data-animate-effect="fadeIn">
+		    			<div class="col-md-7 col-md-push-1 col-md-push-5 col-sm-7 col-sm-push-1 col-sm-push-5">
+			    			<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+						    		<h1 class="fh5co-lead ">POS Machine for Vendor</h1>
+									<h2 class="fh5co-sub-lead ">We have Designed A Standalone POS Machine With Built-in Printer And Touch Screen.Easy Set Up, No Extra Fee.</h2>
+									
+						    	</div>
+						    </div>
+					    </div>
+					    <div class="col-md-4 col-md-pull-7 col-sm-4 col-sm-pull-7 mobile-image">
+					    	<div class="mobile"><img src="images/9.png" alt="Free HTML5 Template by FREEHTML5.co"></div>
+					    </div>
+
+		    		</div>
+		    	</div>
+		    </div>
+
+		   
+		</div>
+	</div>
+
+	<div id="fh5co-our-services" data-section="services">
+		<div class="container">
+			<div class="row row-bottom-padded-sm animate-box">
+				<div class="col-md-12 section-heading text-center">
+					<h2>Our Services</h2>
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<h3>We will provide the payment solution based on your business.</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+
+				<div class="col-md-4">
+					<div class="box animate-box">
+						<div class="icon colored-1"><span><img src="images/002-present.png" alt="icon"></span></div>
+						<h3>Nice Deal</h3>
+						<p>No setup fee, no management fee, and probably the lowest processing rate.</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="box animate-box">
+						<div class="icon colored-4"><span><img src="images/003-folder.png" alt="icon"></span></div>
+						<h3>Security</h3>
+						<p>We keep your business and customers' information safe. Real-time transaction monitoring and our protection policies.</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="box animate-box">
+						<div class="icon colored-2"><span><img src="images/001-computer.png" alt="icon"></span></div>
+						<h3>Advanced Technology</h3>
+						<p>Merchant only need one QR code for both Wechat and Alipay users. Merchant can choose QR code or POS machine. We also provide API to the merchants.</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	
+	<div id="fh5co-product-screenshots" data-section="screenshots">
+
+		<div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeIn">
+				<div class="col-md-12 section-heading text-center">
+					<h2>App Screenshots</h2>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3">
+							<h3>The App In POS Machine Provides Payment Solution Including Tips and Refund.</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		<div class="owl-carousel-center">
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w1.png" alt="w1"></div>
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w2.png" alt="w2"></div>
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w3.png" alt="w3"></div>
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w4.png" alt="w4"></div>
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w5.png" alt="w5"></div>
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w6.png" alt="w6"></div>
+			<div class="animate-box" data-animate-effect="fadeIn"><img src="images/w7.png" alt="w7"></div>
+		</div>
+	</div>
+
+	<div id="fh5co-tour" data-section="tour">
+		<div class="container">
+			<div class="row row-bottom-padded-sm animate-box" data-animate-effect="fadeIn">
+				<div class="col-md-12 section-heading text-center">
+						<h2>Live Demo</h2>
+				
+				</div>
+			</div>
+		
+			<div class="row row-bottom-padded-lg">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<img src="images/onlineshop.png" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">	
+					</div>
+					<div class="col-md-7 col-md-push-1">
+						<h2>Online Shop</h2>
+						<p>Using Wechat Pay for Online Shopping, costumer can easily finish payment by scanning QR code.</p>
+						<p><a href="/phpcartdemo" class="btn btn-primary">Learn more</a></p>	
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="row row-bottom-padded-lg">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="col-md-4 col-md-push-8 animate-box" data-animate-effect="fadeInRight">
+						<img src="images/payment.png" alt="Free HTML5 Bootstrap Template" class="img-responsive img-feature-1">	
+					</div>
+					<div class="col-md-7 col-md-pull-4">
+						<h2>Payment Methods</h2>
+						<p>AlphaPay provides different payment methods for different environment.</p>
+						<p><a href="/phpdemo/common" class="btn btn-primary">Learn more</a></p>	
+					</div>
+					
+				</div>
+			</div>
+
+		</div>
+	</div>
+	
+	
+	<div class="fh5co-cta" style="background-image:url(images/hero.jpg)">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="col-md-8 col-md-offset-2 text-center">
+				<h3>Gorw Up Your Business With AlphaPay.</h3>
+				<p><a href="http://alphapay.ca/applym" class="btn btn-primary btn-outline btn-lg">Get started</a></p>
+			</div>
+		</div>
+	</div>
+
+	<footer id="footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-bottom-padded-sm">
+				 <div class="col-md-3">
+	              <p> Contact us: 1-855-937-7888<br>E-mail: info@alphapay.ca</p>
+	            </div>
+	             <div class="col-md-3">
+	              <p> Address:230-4320 Viking Way<br>Richmond,BC V6V 2L4</p>
+	             </div>
+	            <div class="col-md-6">
+	              <p>CopyRight © 2017-2018. All right reserved by AlphaPay.</p>
+	            </div>
+			</div>
+		
+		</div>
+	</footer>
+	
+	
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
